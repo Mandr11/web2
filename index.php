663f2a1737474cc14b4901e56bab1ec7fcc38abe
Mandri <ado79144@gmail.com>
2025-10-17 10:51:43 +0700

    Add index.php to display server information

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h1>Web2 - Aplikasi 2 </h1>";
+echo "<p>test ubah dari github</p>";
+echo "<p>Server IP: " . ($_SERVER['SERVER_ADDR'] ?? 'unknown') . " | Port: 2000</p>";
+echo "<p>Waktu server: " . date('Y-m-d H:i:s') . "</p>";
+?>
